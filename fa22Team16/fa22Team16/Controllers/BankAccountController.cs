79200932bd11ee77892414f649eed9162b527c98
glorialwang <111816444+glorialwang@users.noreply.github.com>
2022-11-29 00:51:35 -0600

    commit

--- fa22Team16/fa22Team16/Controllers/BankAccountController.cs
+++ fa22Team16/fa22Team16/Controllers/BankAccountController.cs
@@ -59,31 +59,54 @@ namespace fa22Team16
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Customer")]
         public async Task<IActionResult> Create([Bind("AccountID,AccountNumber,Balance,AccountType,ActiveStatus")] BankAccount account)
         {
-            if (ModelState.IsValid)
+            account.AccountNumber = Utilities.GenerateNextAccountNumber.GetNextAccountNumber(_context);
+
+           
+            account.appUser = _context.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+
+            
+            if (ModelState.IsValid == false)
             {
-                _context.Add(account);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return View(account);
             }
-            return View(account);
+
+            
+            _context.Add(account);
+            await _context.SaveChangesAsync();
+
+            
+            return RedirectToAction("Create", "AccountDetails", new { accountID = account.BankAccountID });
         }
 
         // GET: BankAccount/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        // public async Task<IActionResult> Edit(int? id);
+        [Authorize(Roles = "Admin")]
+        public IActionResult Edit(int? id)
         {
-            if (id == null || _context.BankAccounts == null)
+            //did not specify an account to edit
+            if (id == null)
             {
-                return NotFound();
+                return View("Error", new String[] { "Please specify an account to edit" });
+            }
+            BankAccount BankAccount = _context.BankAccounts.FirstOrDefault(m => m.BankAccountID == id);//
+            
+            //account was not found in the database
+            if (BankAccount == null)
+            {
+                return View("Error", new String[] { "This account was not found!" });
             }
 
-            var account = await _context.BankAccounts.FindAsync(id);
-            if (account == null)
+            //account does not belong to this user
+            if (User.IsInRole("Customer") && BankAccount.appUser.UserName != User.Identity.Name)
             {
-                return NotFound();
+                return View("Error", new String[] { "You are not authorized to edit this account!" });
             }
-            return View(account);
+
+            return View(BankAccount);
+            //return RedirectToAction("Edit", "BankAccount", new { accountID = BankAccount.BankAccountID });
         }
 
         // POST: BankAccount/Edit/5
@@ -91,76 +114,83 @@ namespace fa22Team16
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("AccountID,AccountNumber,Balance,AccountType,ActiveStatus")] BankAccount account)
+        public async Task<IActionResult> Edit(int id, [Bind("ActiveStatus")] BankAccount account)
         {
+            //this is a security measure to make sure the user is editing the correct account
             if (id != account.BankAccountID)
             {
-                return NotFound();
+                return View("Error", new String[] { "There was a problem editing this account. Try again!" });
             }
 
-            if (ModelState.IsValid)
+            //if there is something wrong with this order, try again
+            if (ModelState.IsValid == false)
             {
-                try
-                {
-                    _context.Update(account);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AccountExists(account.BankAccountID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                return View(account);
             }
-            return View(account);
+
+            //if code gets this far, update the record
+            try
+            {
+                //find the record in the database
+                BankAccount dbBankAccount = _context.BankAccounts.Find(account.BankAccountID);
+
+                _context.Update(dbBankAccount);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return View("Error", new String[] { "There was an error updating this account!", ex.Message });
+            }
+
+            //send the user to the Orders Index page.
+            return RedirectToAction(nameof(Index));
         }
 
-        //// GET: BankAccount/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null || _context.Accounts == null)
-        //    {
-        //        return NotFound();
-        //    }
 
-        //    var account = await _context.Accounts
-        //        .FirstOrDefaultAsync(m => m.AccountID == id);
-        //    if (account == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(account);
-        //}
-
-        //// POST: BankAccount/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    if (_context.Accounts == null)
-        //    {
-        //        return Problem("Entity set 'AppDbContext.Accounts'  is null.");
-        //    }
-        //    var account = await _context.Accounts.FindAsync(id);
-        //    if (account != null)
-        //    {
-        //        _context.Accounts.Remove(account);
-        //    }
-            
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        private bool AccountExists(int id)
-        {
-          return _context.BankAccounts.Any(e => e.BankAccountID == id);
-        }
     }
 }
+
+//// GET: BankAccount/Delete/5
+//public async Task<IActionResult> Delete(int? id)
+//{
+//    if (id == null || _context.Accounts == null)
+//    {
+//        return NotFound();
+//    }
+
+//    var account = await _context.Accounts
+//        .FirstOrDefaultAsync(m => m.AccountID == id);
+//    if (account == null)
+//    {
+//        return NotFound();
+//    }
+
+//    return View(account);
+//}
+
+//// POST: BankAccount/Delete/5
+//[HttpPost, ActionName("Delete")]
+//[ValidateAntiForgeryToken]
+//public async Task<IActionResult> DeleteConfirmed(int id)
+//{
+//    if (_context.Accounts == null)
+//    {
+//        return Problem("Entity set 'AppDbContext.Accounts'  is null.");
+//    }
+//    var account = await _context.Accounts.FindAsync(id);
+//    if (account != null)
+//    {
+//        _context.Accounts.Remove(account);
+//    }
+
+//    await _context.SaveChangesAsync();
+//    return RedirectToAction(nameof(Index));
+//}
+
+
+//private bool AccountExists(int id)
+ //       {
+ //        return _context.BankAccounts.Any(e => e.BankAccountID == id);
+  //      }
+   // }
+//}
