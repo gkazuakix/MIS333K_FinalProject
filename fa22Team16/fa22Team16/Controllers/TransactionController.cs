ee14b3d9a3786fe18f87bb22d876ee8dd227b95f
sofiachang13 <sofia.chang@utexas.edu>
2022-12-01 17:38:24 -0600

    transactions

--- fa22Team16/fa22Team16/Controllers/TransactionController.cs
+++ fa22Team16/fa22Team16/Controllers/TransactionController.cs
@@ -25,7 +25,27 @@ namespace fa22Team16
         
         public async Task<IActionResult> Index()
         {
-              return View(await _context.Transactions.ToListAsync());
+            List<Transaction> transactions = new List<Transaction>();
+            ////if (User.IsInRole("Customer"))
+            ////{
+            ////    foreach (Transaction transaction in _context.Transactions)
+            ////    {
+            ////        if (transaction.Account != null)
+            ////        {
+            ////            transactions = _context.Transactions.Where(o => o.Account.appUser.UserName == User.Identity.Name).ToList();
+            ////        }
+            ////        else if (transaction.StockPortfolio != null)
+            ////        {
+            ////            transactions = _context.Transactions.Where(o => o.Account.appUser.UserName == User.Identity.Name).ToList();
+            ////        }
+            ////    }
+            ////}
+            transactions = _context.Transactions.Where(o => o.Account.appUser.UserName == User.Identity.Name).ToList();
+
+            return View(transactions);
+            //return View(await _context.Transactions.ToListAsync());
+            //create a new instance of the RegistrationDetail class
+            
         }
 
         // GET: Transaction/Details/5
@@ -124,13 +144,38 @@ namespace fa22Team16
         public async Task<IActionResult> DepositCreate()
         {
             ViewBag.AllAccounts = GetAllAccountsSelectList();
-            return View();
+            DepositCreateViewModel dpvm = new DepositCreateViewModel();
+            dpvm.Date = DateTime.Now;
+            //find the registration that should be associated with this registration
+            //BankAccount dbBankAccount = _context.BankAccounts.Find(TransactionID);
+
+            //set the new registration detail's registration equal to the registration you just found
+            //dpvm.Account = dbBankAccount;
+
+
+            //Transaction transaction = new Transaction();
+
+            ////find the registration that should be associated with this registration
+            //BankAccount dbBankAccount = _context.BankAccounts.Find(TransactionID);
+
+            ////set the new registration detail's registration equal to the registration you just found
+            //transaction.Account = dbBankAccount;
+
+            ////populate the ViewBag with a list of existing courses
+            //ViewBag.AllBankAccounts = GetAllAccountsSelectList();
+
+            ////pass the newly created registration detail to the view
+            //return View(transaction);
+
+            return View(dpvm);
         }
 
         // Post: Transaction/Deposit Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Transaction transaction, int AccountNumber, int[] SelectedAccount)
+        //public async Task<IActionResult> Create(DepositCreateViewModel transaction, int AccountNumber, int[] SelectedAccount)
+        public async Task<IActionResult> Create(DepositCreateViewModel transaction, Int64 SelectedAccount)
+
         {
             if (ModelState.IsValid)
             {
@@ -142,8 +187,11 @@ namespace fa22Team16
                 {
                     transaction.Account = _context.BankAccounts.Find(SelectedAccount);
                     transaction.Account.Balance = transaction.Account.Balance + transaction.Amount;
-                    transaction.Date = DateTime.Now;
-                    _context.Add(transaction);
+                    Transaction deposit = new Transaction();
+                    deposit.Date = transaction.Date;
+                    deposit.Amount = transaction.Amount;
+                    deposit.Comments = transaction.Comments;
+                    _context.Add(deposit);
                     await _context.SaveChangesAsync();
                 }
              return RedirectToAction(nameof(Index));
@@ -151,7 +199,7 @@ namespace fa22Team16
             }
 
             // await _context.SaveChangesAsync();
-            return RedirectToAction("Index", "Transaction");
+            return View(transaction);
         }
 
         public async Task<IActionResult> WithdrawCreate(Transaction transaction)
@@ -266,6 +314,15 @@ namespace fa22Team16
         {
             //Get the list of months from the database
             List<BankAccount> bankAccountList = _context.BankAccounts.ToList();
+            //List<BankAccount> bankAccountList = new List<BankAccount>();
+
+            //foreach (BankAccount bankAccount in _context.BankAccounts)
+            //{
+            //    if (bankAccount.appUser.Email == User.Identity.Name)
+            //    {
+            //        bankAccountList.Add(bankAccount);
+            //    }
+            //}
 
             //convert the list to a SelectList by calling SelectList constructor
             //MonthID and MonthName are the names of the properties on the Month class
